1debb0d664269e5a7a1801e49fe0b58e825ebb16
Leonardo Horacio <leonardohoracio81@gmail.com>
2022-08-14 05:16:51 -0300

    exercicio 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		System.out.println(fatorial(n));
+	}
+
+	private static int fatorial(int n) {
+		if (n == 0) {
+			return 1;
+		}else {
+			return n * fatorial (n-1);
+			
+		}
+	}
+
+}
